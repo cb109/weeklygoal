43abbf892243d20685b482cd372125e10bd39a5e
Christoph Buelter <buelter.christoph@gmail.com>
2020-04-18 12:51:20 +0200

    Fix STATIC_ROOT is unset

--- weeklygoal/settings.py
+++ weeklygoal/settings.py
@@ -116,6 +116,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = "/static/"
-
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR, "public")
+STATIC_URL = "/static/"
