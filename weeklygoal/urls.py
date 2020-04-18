a6e3059745879c1ffe7a0b86cdf01d537f7a0653
Christoph Buelter <buelter.christoph@gmail.com>
2020-04-18 14:13:49 +0200

    Integrate the frontend prototype

--- weeklygoal/urls.py
+++ weeklygoal/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("app/", app, name="app"),
+    path("", app, name="app"),
 ]
 
 # Serve user-uploaded files.
